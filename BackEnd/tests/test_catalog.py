3533d8550d3ed97d683523edebcb2b91b3b33ffa
Sebas262002 <sebas-churuchumbi@hotmail.com>
2026-05-15 07:41:46 -0500

    1552026

--- BackEnd/tests/test_catalog.py
+++
@@ -1,6 +0,0 @@
-from app.services.catalog_service import CatalogService
-
-
-def test_catalog_loads_relations():
-    service = CatalogService.from_path("./anexo_c_clasificacion.json")
-    assert len(service.get_relations()) == 47
